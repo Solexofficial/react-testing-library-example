7547c27bea8d3ca4f798f8f7607229ffa7519753
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-23 23:32:13 +0300

    feat: add app components tests

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const helloWorldEl = screen.getByText(/Hello world/i);
+  const btn = screen.getByRole('button');
+  const input = screen.getByPlaceholderText(/input value/i);
+  expect(helloWorldEl).toBeInTheDocument();
+  expect(btn).toBeInTheDocument();
+  expect(input).toMatchSnapshot();
 });
