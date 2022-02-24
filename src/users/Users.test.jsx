0dddf9a6710001ebad1114c4010b31b02dd25675
Mark Pokrovsky <solexofficial@yandex.ru>
2022-02-24 22:59:56 +0300

    feat: add async fn tests

---
+++ src/users/Users.test.jsx
@@ -0,0 +1,31 @@
+import { render, screen } from '@testing-library/react';
+import Users from './Users';
+import axios from 'axios';
+
+jest.mock('axios');
+
+describe('USERS TEST', () => {
+  let response;
+  beforeEach(() => {
+    response = {
+      data: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+        },
+        {
+          id: 2,
+          name: 'Ervin Howell',
+        },
+      ],
+    };
+  });
+
+  test('render users', async () => {
+    axios.get.mockReturnValue(response);
+    render(<Users />);
+    const users = await screen.findAllByTestId('user-item');
+    expect(users.length).toBe(3);
+    expect(axios.get).toBeCalledTimes(1);
+  });
+});
