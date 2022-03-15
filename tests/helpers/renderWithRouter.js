b12646437d1b24a48c5420da49e8b83feea38d2f
Mark Pokrovsky <solexofficial@yandex.ru>
2022-03-15 23:10:04 +0300

    install storybook

--- tests/helpers/renderWithRouter.js
+++ tests/helpers/renderWithRouter.js
@@ -1,6 +1,6 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import AppRouter from '../../router/AppRouter';
+import AppRouter from '../../src/router/AppRouter';
 
 export const renderWithRouter = (component, initialRoute = '/') => {
   return render(
